d9ed583fd4460cc663ea9ca49e6eb3201278ca11
Saikal Kuttubekova <saikal.kuttubekova.on@gmail.com>
2022-01-29 18:13:29 +0600

    add task6

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+document.body.style.background = "hsl(217, 54%, 11%)";  //body
+// document.body.style.display = "flex";
+//document.body.style.minWidth = '1440px';
+//document.body.style.fontFamily = 'Outfit, sans-serif';
+//document.body.style.fontSize = '18px';
+//document.body.style.margin = '0';
+
+
+// wrapper
+const wrapper = document.createElement("div");
+wrapper.id = "wrapper";
+document.body.append(wrapper);
+wrapper.style.marginTop = "100px";
+wrapper.style.marginBottom = "100px";
+wrapper.style.fontFamily = "Outfit";
+wrapper.style.width = "340px";
+wrapper.style.height = "600px";
+wrapper.style.backgroundColor = "hsl(216, 50%, 16%)";
+wrapper.style.borderRadius = "15px";
+wrapper.style.margin = "auto";
+wrapper.style.boxShadow = "0px 15px 10px 15px hsl(233, 45%, 10%)";
