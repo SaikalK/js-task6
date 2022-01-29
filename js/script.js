a6accbfec9ec06072e6af3d8af9bdbff92dd7a51
Saikal Kuttubekova <saikal.kuttubekova.on@gmail.com>
2022-01-29 18:16:49 +0600

    add task6 new

--- js/script.js
+++ js/script.js
@@ -20,3 +20,25 @@ wrapper.style.backgroundColor = "hsl(216, 50%, 16%)";
 wrapper.style.borderRadius = "15px";
 wrapper.style.margin = "auto";
 wrapper.style.boxShadow = "0px 15px 10px 15px hsl(233, 45%, 10%)";
+//img
+const image = document.createElement("img");
+wrapper.append(image);
+image.setAttribute("src", "./images/image-equilibrium.jpg");
+image.style.width = "300px";
+image.style.height = "300px";
+image.style.borderRadius = "15px";
+image.style.marginLeft = "20px";
+image.style.marginTop = "20px";
+
+//h3
+const title = document.createElement("h3");
+wrapper.append(title);
+title.innerText = "Equilibrium #3429";
+title.style.color = "white";
+title.style.fontSize = "21px";
+title.style.fontFamily = "Outfit";
+title.style.fontWeight = "600";
+title.style.marginLeft = "20px";
+title.style.marginTop = "20px";
+title.style.textDecoration = "none";
+title.style.display = "Block";
