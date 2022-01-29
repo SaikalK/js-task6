61e5600490858f80a92d71465b3df35e0bca1f9f
Saikal Kuttubekova <saikal.kuttubekova.on@gmail.com>
2022-01-29 18:22:02 +0600

    add task6 finish

--- js/script.js
+++ js/script.js
@@ -42,3 +42,101 @@ title.style.marginLeft = "20px";
 title.style.marginTop = "20px";
 title.style.textDecoration = "none";
 title.style.display = "Block";
+
+
+//paragraph
+const text = document.createElement("p");
+wrapper.append(text);
+text.innerText = "Our Equilibrium collection promotes balance and calm";
+text.style.fontSize = "18px";
+text.style.fontFamily = "Outfit";
+text.style.fontWeight = "300";
+text.style.color = "hsl(215, 32%, 35%)";
+text.style.marginLeft = "20px";
+
+//footer-sides
+const innerWrapper = document.createElement("ul");
+wrapper.append(innerWrapper);
+innerWrapper.className = "innerWrapper";
+innerWrapper.style.display = "flex";
+innerWrapper.style.justifyContent = "space-around";
+innerWrapper.style.listStyle = "none";
+
+//balance
+const innerItem = document.createElement("li");
+innerWrapper.append(innerItem);
+innerItem.innerText = "0.041ETH";
+innerItem.style.color = "hsl(178, 100%, 50%)";
+innerItem.style.fontFamily = "Outfit";
+innerItem.style.fontWeight = "400";
+innerItem.style.fontSize = "18px";
+innerItem.style.marginLeft = "-50px";
+
+
+//balanceIcon
+const icon = document.createElement("img");
+icon.setAttribute("src", "images/icon-ethereum.svg");
+innerItem.append(icon);
+icon.style.top = "466px";
+icon.style.left = "525px";
+icon.style.position = "absolute";
+
+
+//time
+const innerItem2 = document.createElement("li");
+innerWrapper.append(innerItem2);
+innerItem2.innerText = "3 days left";
+innerItem2.style.fontFamily = "Outfit";
+innerItem2.style.fontWeight = "400";
+innerItem2.style.fontSize = "18px";
+innerItem2.style.color = "hsl(215, 51%, 70%)";
+
+//timeIcon
+const icon2 = document.createElement("img");
+icon2.setAttribute("src", "images/icon-clock.svg");
+innerItem2.append(icon2);
+icon2.style.top = "466px";
+icon2.style.left = "695px";
+icon2.style.position = "absolute";
+
+
+//line
+const line = document.createElement("hr");
+wrapper.append(line);
+line.style.width = "300px";
+line.style.border = ".2px solid hsl(215, 32%, 35%) ";
+line.style.marginTop = "20px";
+
+
+//ava
+const ava = document.createElement("img");
+wrapper.append(ava);
+ava.setAttribute("src", " images/image-avatar.png");
+ava.style.width = "40px";
+ava.style.border = "1px solid white";
+ava.style.borderRadius = "50%";
+ava.style.marginLeft = "20px";
+ava.style.marginTop = "15px";
+
+
+
+//span
+const span = document.createElement("span");
+wrapper.append(span);
+span.innerText = "Creation of";
+span.style.color = "hsl(215, 32%, 40%)";
+span.style.fontFamily = "Outfit";
+span.style.fontWeigth = "400";
+span.style.fontSize = "18px";
+span.style.marginLeft = "75px";
+span.style.marginTop = "-32px";
+span.style.display = "block";
+
+//cite
+const cite = document.createElement("cite");
+span.appendChild(cite);
+cite.innerText = "Jules Wyvern";
+cite.style.color = "white";
+cite.style.textDecoration = "none";
+cite.style.fontWeight = "400";
+cite.style.marginLeft = "8px";
